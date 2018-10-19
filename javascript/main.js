3bba1ed752df53d284cb21b9f4b987cf4b8ae47b
takasugimeng <35606663+takasugimeng@users.noreply.github.com>
2018-10-19 13:50:07 -0400

    Initial commit

---
+++ javascript/main.js
@@ -0,0 +1,41 @@
+// ========================================
+// Your JavaScript goes here
+// ========================================
+
+// If you want to use the toggleClass
+// function, uncomment the function below
+// and replace '.myClass' and
+// 'class-to-toggle' with your own data.
+// Both parameters must be in quotes.
+
+// toggleClass('.myClass', 'class-to-toggle');
+
+
+
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"
+
+function vidPlay() {
+    video1.play();
+}
+function vidPause() {
+	video1.pause();
+}
+
+
+// top button
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
